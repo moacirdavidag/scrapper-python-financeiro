7e580fe6190e768f3b98389a3b71fabde8edf9f1
moacirdavidag <moacirdavidag@gmail.com>
2025-09-22 13:41:56 -0300

    feat: Scrapper com selenium e BeautifulSoup dos ícones das ações do Trading View

--- index.py
+++ index.py
@@ -1,23 +1,43 @@
-# Vai ser o início do scrapper dos ícones de ações e moedas
-# Poder Mercado
-# Time Rocket
-# Moacir David
-# Marizópolis > BSB
-# Governo Poderal360
-
-import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+import time
+import os
+import requests
 
-url = 'https://br.tradingview.com/screener/'
+options = Options()
+options.add_argument("--headless")
+driver = webdriver.Chrome(options=options)
 
-print('[INFO] Iniciando requisição a página da Trading View...')
+print('[INFO] Iniciando requisição com Selenium a página da TradeView...')
+driver.get("https://br.tradingview.com/screener/")
+time.sleep(5)
 
-response = requests.get(url)
+html = driver.page_source
+soup = BeautifulSoup(html, "html.parser")
 
-print('[INFO] Requisição concluída com status ' + response.status_code)
+tbody = soup.find("tbody", {"data-testid": "selectable-rows-table-body"})
+trs = tbody.find_all("tr") if tbody else []
 
-html_pagina = response.content
+print(f"[INFO] Encontrados {len(trs)} TRs dentro do tbody alvo")
 
-print('[INFO] Conteúdo da página passado para HTML')
+os.makedirs("assets/icones/acoes", exist_ok=True)
 
-print(html_pagina)
+for tr in trs: 
+    print('[INFO] Pegando célula da logo e o nome da ação')
+    td = tr.find('td')
+    if td:
+        nome_acao_tag = td.find("a", class_="tickerNameBox-GrtoTeat")
+        descricao_tag = td.find("sup", class_="tickerDescription-GrtoTeat")
+        nome_acao = nome_acao_tag.get_text(strip=True) if nome_acao_tag else None
+        descricao = descricao_tag.get_text(strip=True) if descricao_tag else None
+        print("[SUCCESS] Coletado: ", nome_acao, "-", descricao)
+        img_tag = td.find("img", class_="logo-PsAlMQQF")
+        if img_tag and img_tag.get("src"):
+            url_svg = img_tag["src"]
+            response = requests.get(url_svg)
+            if response.status_code == 200:
+                path = f"assets/icones/acoes/{nome_acao}.svg"
+                with open(path, "wb") as f:
+                    f.write(response.content)
+                print(f"[SUCCESS] Ícone salvo em {path}")
