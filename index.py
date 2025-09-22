dd2f8e2b7fae72fa7836aaff5914bbdb8d1a937e
moacirdavidag <moacirdavidag@gmail.com>
2025-09-22 11:27:34 -0300

    feat: Arquivo de dependências e primeira requisição

--- index.py
+++ index.py
@@ -5,3 +5,19 @@
 # Marizópolis > BSB
 # Governo Poderal360
 
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://br.tradingview.com/screener/'
+
+print('[INFO] Iniciando requisição a página da Trading View...')
+
+response = requests.get(url)
+
+print('[INFO] Requisição concluída com status ' + response.status_code)
+
+html_pagina = response.content
+
+print('[INFO] Conteúdo da página passado para HTML')
+
+print(html_pagina)
